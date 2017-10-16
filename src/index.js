79388759a5c04ac31efb26cc54b69f14a5ac16d9
aoropesa <aoropesa@fvi.edu>
2017-10-16 19:34:04 -0400

    first commit react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function ErrorBox({children}){
+  return(
+    <div className="alert alert-danger error-box">
+      <i className="fa fa-exclamation-triangle"/>
+      {children}
+    </div>
+  );
+}
+
+ReactDOM.render(<ErrorBox>The world is ending</ErrorBox>, document.getElementById('root'));
